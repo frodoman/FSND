f0bcf58c309c4329cfe2dbbcc14bb7ab4cab2585
frodoman <my_adress_2003@hotmail.com>
2020-06-21 15:49:12 +0100

    Add many to many relationship between Venue and Artist.

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -32,24 +32,6 @@ migrate = Migrate(app, db)
 # Models.
 #----------------------------------------------------------------------------#
 
-class Venue(db.Model):
-    __tablename__ = 'Venue'
-
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    city = db.Column(db.String(120))
-    state = db.Column(db.String(120))
-    address = db.Column(db.String(120))
-    phone = db.Column(db.String(120))
-    genres = db.Column(db.String(120))
-    image_link = db.Column(db.String(500))
-    facebook_link = db.Column(db.String(120))
-
-    def __repr__(self):
-      return f'<Venue {self.id} {self.name} {self.city}>'
-
-    # TODO: implement any missing fields, as a database migration using Flask-Migrate
-
 class Artist(db.Model):
     __tablename__ = 'Artist'
 
@@ -67,8 +49,48 @@ class Artist(db.Model):
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
 
 # TODO Implement Show and Artist models, and complete all model relationships and properties, as a database migration.
+'''
+class Show(db.Model):
+  __tablename__ = 'Show'
 
-db.create_all()
+  id = db.Column(db.Integer, primary_key=True)
+  venu_id = db.Column(db.Integer, db.ForeignKey('Vnue.id'))
+  artist_id = db.Column(db.Integer, db.ForeignKey('Artist.id'))
+
+  name = db.Column(db.String)
+  time = db.Column(db.DateTime)
+
+  def __repr__(self):
+    return f'<Venue {self.name} {self.time}>'
+'''
+
+show_item = db.Table('show_item',
+    db.Column('venu_id', db.Integer, db.ForeignKey('Venue.id'), primary_key=True),
+    db.Column('artist_id', db.Integer, db.ForeignKey('Artist.id'), primary_key=True)
+)
+
+class Venue(db.Model):
+    __tablename__ = 'Venue'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    city = db.Column(db.String(120))
+    state = db.Column(db.String(120))
+    address = db.Column(db.String(120))
+    phone = db.Column(db.String(120))
+    genres = db.Column(db.String(120))
+    image_link = db.Column(db.String(500))
+    facebook_link = db.Column(db.String(120))
+
+    artists = db.relationship('Artist', secondary=show_item, 
+            backref=db.backref('venues', lazy=True))
+      
+    def __repr__(self):
+      return f'<Venue {self.id} {self.name} {self.city}>'
+
+    # TODO: implement any missing fields, as a database migration using Flask-Migrate
+
+#db.create_all()
 #----------------------------------------------------------------------------#
 # Filters.
 #----------------------------------------------------------------------------#
