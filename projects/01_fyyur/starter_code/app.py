d9050466b964175db650f8cc237f585dc573c457
frodoman <my_adress_2003@hotmail.com>
2020-06-24 18:07:01 +0100

    Add missing functions for vernue routes.

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -61,122 +61,15 @@ def index():
 #  Create Venue
 #  ----------------------------------------------------------------
 
-@app.route('/venues/create', methods=['GET'])
-def create_venue_form():
-  form = VenueForm()
-  return render_template('forms/new_venue.html', form=form)
-
-@app.route('/venues/create', methods=['POST'])
-def create_venue_submission():
-  # TODO: insert form data as a new Venue record in the db, instead
-  # TODO: modify data to be the data object returned from db insertion
-  error = False
-  form = request.form
-
-  try:
-    venu = Venue(name=form['name'], 
-                  city=form['city'], 
-                  state=form['state'],
-                  address=form['address'],
-                  phone=form['phone'])
- 
-    db.session.add(venu)
-    db.session.commit()
-  except:
-    error = True
-    db.session.rollback()
-    flash( sys.exc_info())
-  finally:
-    db.session.close()
-
-  if error == False:
-    flash('Venue ' + form['name'] + ' was successfully listed!')
-  
-  return render_template('pages/home.html')
-
   
   # on successful db insert, flash success
   
   # TODO: on unsuccessful db insert, flash an error instead.
   # e.g., 
   # see: http://flask.pocoo.org/docs/1.0/patterns/flashing/
-  
-
-@app.route('/venues/<venue_id>/delete', methods=['POST'])
-def delete_venue(venue_id):
-  # TODO: Complete this endpoint for taking a venue_id, and using
-  # SQLAlchemy ORM to delete a record. Handle cases where the session commit could fail.
-
-  # BONUS CHALLENGE: Implement a button to delete a Venue on a Venue Page, have it so that
-  # clicking that button delete it from the db then redirect the user to the homepage
-  error = False
-  venue_name = ""
-
-  try:
-      venue = Venue.query.get(venue_id)
-      venue_name = venue.name
-
-      for show in venue.shows:
-        venue.shows.remove(show)
-
-      # Show table
-      shows = Show.query.filter(venue_id==venue_id)
-      showIds = [oneShow.id for oneShow in shows]
-
-      # clean Artist table
-      for oneShow in shows:
-        artist = Artist.query.get(oneShow.artist_id)
-        for artistShow in artist.shows:
-          if artistShow.id in showIds:
-            artist.shows.remove(artistShow)
-
-      # clean Show table
-      shows.delete()
-      
-      # clean Venue table
-      db.session.delete(venue)
-      db.session.commit()
-  except():
-      db.session.rollback()
-      error = True
-  finally:
-      db.session.close()
-
-  if error:
-    flash(sys.exc_info())
-  else:
-    flash('Venue ' + venue_name + 'deleted!')
-  
-  return render_template('pages/home.html')
 
 #  Artists
 #  ----------------------------------------------------------------
-
-@app.route('/venues/<int:venue_id>/edit', methods=['GET'])
-def edit_venue(venue_id):
-  form = VenueForm()
-  venue={
-    "id": 1,
-    "name": "The Musical Hop",
-    "genres": ["Jazz", "Reggae", "Swing", "Classical", "Folk"],
-    "address": "1015 Folsom Street",
-    "city": "San Francisco",
-    "state": "CA",
-    "phone": "123-123-1234",
-    "website": "https://www.themusicalhop.com",
-    "facebook_link": "https://www.facebook.com/TheMusicalHop",
-    "seeking_talent": True,
-    "seeking_description": "We are on the lookout for a local artist to play every two weeks. Please call us.",
-    "image_link": "https://images.unsplash.com/photo-1543900694-133f37abaaa5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60"
-  }
-  # TODO: populate form with values from venue with ID <venue_id>
-  return render_template('forms/edit_venue.html', form=form, venue=venue)
-
-@app.route('/venues/<int:venue_id>/edit', methods=['POST'])
-def edit_venue_submission(venue_id):
-  # TODO: take values from the form submitted, and update existing
-  # venue record with ID <venue_id> using the new attributes
-  return redirect(url_for('show_venue', venue_id=venue_id))
 
 #  Shows
 #  ----------------------------------------------------------------
