2f1b82c96d6856e9803a630ec010e7e83e49bcba
frodoman <my_adress_2003@hotmail.com>
2020-06-26 16:11:41 +0100

    Remove useless codes.

--- projects/01_fyyur/starter_code/route_artists.py
+++ projects/01_fyyur/starter_code/route_artists.py
@@ -14,17 +14,6 @@ def create_artist_form():
 @app.route('/artists')
 def artists():
   # TODO: replace with real data returned from querying the database
-  data=[{
-    "id": 4,
-    "name": "Guns N Petals",
-  }, {
-    "id": 5,
-    "name": "Matt Quevedo",
-  }, {
-    "id": 6,
-    "name": "The Wild Sax Band",
-  }]
-
   artists = Artist.query.with_entities(Artist.id, Artist.name).order_by(Artist.name).all()
 
   return render_template('pages/artists.html', artists=artists)
