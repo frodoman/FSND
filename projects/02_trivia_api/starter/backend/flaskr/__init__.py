46a59994d7a787c3c11172da00b7491ddac65340
frodoman <my_adress_2003@hotmail.com>
2020-07-16 17:16:19 +0100

    Correct category id for api and image name.

--- projects/02_trivia_api/starter/backend/flaskr/__init__.py
+++ projects/02_trivia_api/starter/backend/flaskr/__init__.py
@@ -17,8 +17,8 @@ def create_app(test_config=None):
   '''
   @TODO: Set up CORS. Allow '*' for origins. Delete the sample route after completing the TODOs
   '''
-  #CORS(app, resources={r"/api/*": {"origins": "*"}})
-  CORS(app)
+  CORS(app, resources={r"/api/*": {"origins": "*"}})
+  #CORS(app)
 
   '''
   @TODO: Use the after_request decorator to set Access-Control-Allow
